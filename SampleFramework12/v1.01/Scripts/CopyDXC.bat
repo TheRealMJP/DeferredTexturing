bf815e578b69738a6c2f906a39aab2e47640cba9
MJP <mpettineo@gmail.com>
2017-05-01 01:48:45 -0700

    Updated the readme, and improved the CopyDXC script

--- SampleFramework12/v1.01/Scripts/CopyDXC.bat
+++ SampleFramework12/v1.01/Scripts/CopyDXC.bat
@@ -1,5 +1,5 @@
 @echo off
 
-xcopy %~dp0..\..\..\..\DirectXShaderCompiler\Include\dxc\dxcapi.h %~dp0..\..\..\Externals\DXCompiler\Include\ /y
-xcopy %~dp0..\..\..\..\DirectXShaderCompiler\build\Release\lib\dxcompiler.lib %~dp0..\..\..\Externals\DXCompiler\Lib\ /y
-xcopy %~dp0..\..\..\..\DirectXShaderCompiler\build\Release\bin\dxcompiler.* %~dp0..\..\..\Externals\DXCompiler\Bin\ /y
+xcopy %HLSL_SRC_DIR%\Include\dxc\dxcapi.h %~dp0..\..\..\Externals\DXCompiler\Include\ /y
+xcopy %HLSL_BLD_DIR%\Release\lib\dxcompiler.lib %~dp0..\..\..\Externals\DXCompiler\Lib\ /y
+xcopy %HLSL_BLD_DIR%\Release\bin\dxcompiler.* %~dp0..\..\..\Externals\DXCompiler\Bin\ /y
